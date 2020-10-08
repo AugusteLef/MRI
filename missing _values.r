d62377fd6ca2443f1061deb01463e129b4338c6e
Auguste Lefevre <auguste.lefevre@epfl.ch>
2020-10-08 17:18:38 +0200

    add R script for missing value and csv results

---
+++ missing _values.r
@@ -0,0 +1,53 @@
+# Filling the missing values
+
+#With mean
+#Training set
+x_train = read.csv("X_train.csv") #read csv file
+x_train
+for(i in 1:ncol(x_train)){
+  x_train[is.na(x_train[,i]), i] <- mean(x_train[,i], na.rm = TRUE)
+}
+
+x_train
+
+#Testing set
+x_test = read.csv("X_test.csv") #read csv file
+x_test
+for(i in 1:ncol(x_train)){
+  x_test[is.na(x_test[,i]), i] <- mean(x_test[,i], na.rm = TRUE)
+}
+
+x_test
+
+#write to csv
+write.csv(x_train, "x_train_mean.csv", row.names = FALSE)
+write.csv(x_test, "x_test_mean.csv", row.names = FALSE)
+
+#########################################################################################
+
+
+#With median
+#Training set
+x_train = read.csv("X_train.csv") #read csv file
+x_train
+for(i in 1:ncol(x_train)){
+  x_train[is.na(x_train[,i]), i] <- median(x_train[,i], na.rm = TRUE)
+}
+
+x_train
+
+#Testing set
+x_test = read.csv("X_test.csv") #read csv file
+x_test
+for(i in 1:ncol(x_train)){
+  x_test[is.na(x_test[,i]), i] <- median(x_test[,i], na.rm = TRUE)
+}
+
+x_test
+
+#write to csv
+write.csv(x_train, "x_train_median.csv", row.names = FALSE)
+write.csv(x_test, "x_test_median.csv", row.names = FALSE)
+
+
+######################################################################################
